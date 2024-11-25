e2f301e8fdfe3ca7ddcc5eee0205eca5d83170a6
Nicholas Costa <nicholascostadev@gmail.com>
2024-11-25 19:30:16 -0300

    feat: add website url to cors

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Predict Betting Expenses - API")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://a3-matematica-react.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
